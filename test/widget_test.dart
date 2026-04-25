9b81c9724e7695210ee778ecc07e95523a45784c
Mohammad <habibi.mh@gmail.com>
2026-04-25 17:09:07 +0200

    fix: isolate home screen widget test from app state

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,18 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:parsit_finance_tracker_app/features/transactions/domain/entities/finance_transaction.dart';
-import 'package:parsit_finance_tracker_app/features/transactions/domain/repositories/transaction_repository.dart';
-import 'package:parsit_finance_tracker_app/features/transactions/presentation/providers/transaction_usecase_providers.dart';
+import 'package:parsit_finance_tracker_app/features/transactions/presentation/controllers/transactions_controller.dart';
 import 'package:parsit_finance_tracker_app/features/transactions/presentation/screens/home_screen.dart';
 
 void main() {
   testWidgets('Home screen shows app title', (tester) async {
-    final repository = _FakeTransactionRepository();
-
     await tester.pumpWidget(
       ProviderScope(
         overrides: [
-          transactionRepositoryProvider.overrideWithValue(repository),
+          transactionsControllerProvider.overrideWith(
+            () => _FakeTransactionsController(),
+          ),
         ],
         child: const MaterialApp(
           home: HomeScreen(),
@@ -28,13 +27,8 @@ void main() {
   });
 }
 
-class _FakeTransactionRepository implements TransactionRepository {
+class _FakeTransactionsController
+    extends AutoDisposeAsyncNotifier<List<FinanceTransaction>> {
   @override
-  Future<void> addTransaction(FinanceTransaction transaction) async {}
-
-  @override
-  Future<void> deleteTransaction(String id) async {}
-
-  @override
-  Future<List<FinanceTransaction>> getTransactions() async => [];
+  Future<List<FinanceTransaction>> build() async => [];
 }
