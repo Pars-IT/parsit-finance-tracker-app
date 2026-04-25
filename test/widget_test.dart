ba4a0971a457144d2cc286bc045d5db6bf8a1968
Mohammad <habibi.mh@gmail.com>
2026-04-25 17:03:21 +0200

    fix: stabilize widget test with repository override

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,21 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter/material.dart';
+import 'package:parsit_finance_tracker_app/features/transactions/domain/entities/finance_transaction.dart';
+import 'package:parsit_finance_tracker_app/features/transactions/domain/repositories/transaction_repository.dart';
+import 'package:parsit_finance_tracker_app/features/transactions/presentation/providers/transaction_usecase_providers.dart';
 import 'package:parsit_finance_tracker_app/features/transactions/presentation/screens/home_screen.dart';
 
 void main() {
   testWidgets('Home screen shows app title', (tester) async {
+    final repository = _FakeTransactionRepository();
+
     await tester.pumpWidget(
-      const ProviderScope(
+      ProviderScope(
+        overrides: [
+          transactionRepositoryProvider.overrideWithValue(repository),
+        ],
         child: MaterialApp(
           home: HomeScreen(),
         ),
       ),
     );
 
-    await tester.pump();
+    await tester.pumpAndSettle();
 
     expect(find.text('Pars IT Finance Tracker'), findsOneWidget);
     expect(find.text('Recent Transactions'), findsOneWidget);
   });
+}
+
+class _FakeTransactionRepository implements TransactionRepository {
+  @override
+  Future<void> addTransaction(FinanceTransaction transaction) async {}
+
+  @override
+  Future<void> deleteTransaction(String id) async {}
+
+  @override
+  Future<List<FinanceTransaction>> getTransactions() async => [];
 }
